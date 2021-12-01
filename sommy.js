b413bf74eac4374063b4cddb0328617170a6c597
chulocr8v <nkematu5@gmail.com>
2021-12-01 09:45:24 +0100

    update

---
+++ sommy.js
@@ -0,0 +1,17 @@
+const number = Number(prompt('Enter Number'))
+
+if (number < 20) {
+  console.log("Your Number is Less Than 20")
+} else {
+  console.log("Your Number is More Than 20")
+
+}
+
+
+
+for (i = 0; i < 10; i++) {
+  console.log(i)
+  if (i == 5) {
+    console.log('This is Loop 5. I will continue the iteration until I get to 9 ☺ ')
+  }
+}
